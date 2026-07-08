8370f47c63cf77e1d4be79f71e8358f0c8df4dd5
Tim Molteno <tim@elec.ac.nz>
2026-07-08 13:04:52 +1200

    Added a compression to the downloaded catalog_cache

--- download_catalog.sh
+++ download_catalog.sh
@@ -2,3 +2,6 @@
 # After install, login and run the server manually.
 rsync -rvz --exclude .git --exclude .gitignore \
     tart@tart.elec.ac.nz:catalog_cache .
+rm -f catalog_cache.tar*
+tar -cf catalog_cache.tar catalog_cache
+xz -6 catalog_cache.tar
